f8df77392d48505fd6075ffb81d7e7c357933104
Dima <d.d.demin@urfu.me>
2023-12-16 12:01:39 +0500

    Add new files

--- optimizations/JPEG/Program.cs
+++ optimizations/JPEG/Program.cs
@@ -17,7 +17,7 @@ namespace JPEG
             {
                 Console.WriteLine(IntPtr.Size == 8 ? "64-bit version" : "32-bit version");
                 var sw = Stopwatch.StartNew();
-                var fileName = @"MARBLES.bmp";
+                var fileName = @"sample.bmp";
 //				var fileName = "Big_Black_River_Railroad_Bridge.bmp";
                 var compressedFileName = fileName + ".compressed." + Compressor.CompressionQuality;
                 var uncompressedFileName = fileName + ".uncompressed." + Compressor.CompressionQuality + ".bmp";
